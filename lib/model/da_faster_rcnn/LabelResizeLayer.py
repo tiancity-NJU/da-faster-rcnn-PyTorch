f4b6ac4f16159b1b6416bb96bfd57e6e0ba7cdbe
tiancity-NJU <32325193+tiancity-NJU@users.noreply.github.com>
2019-07-21 22:55:12 +0800

    Update LabelResizeLayer.py

--- lib/model/da_faster_rcnn/LabelResizeLayer.py
+++ lib/model/da_faster_rcnn/LabelResizeLayer.py
@@ -50,9 +50,6 @@ class InstanceLabelResizeLayer(nn.Module):
     def forward(self, x,need_backprop):
         feats = x.data.cpu().numpy()
         lbs = need_backprop.data.cpu().numpy()
-
-
-
         resized_lbs = np.ones((feats.shape[0], 1), dtype=np.float32)
         for i in range(lbs.shape[0]):
             resized_lbs[i*self.minibatch:(i+1)*self.minibatch] = lbs[i]
